6987faf8ddbe852ac0a277c1a667b6baa22d507e
Anastasiia Deviatkina <anastasia.deviatkina@gmail.com>
2023-11-07 11:59:04 +0100

    Add the method renderInMenu by using the algoritm (Dodano metode renderInMenu używając wcześniej dodanego algorytmu)

--- src/js/script.js
+++ src/js/script.js
@@ -66,14 +66,18 @@ const select = {
     renderInMenu(){
       const thisProduct = this;
 
-      /* enerate HTML based on template */
-
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      
       /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
       /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
       
       /* add element to menu */
-
+      menuContainer.appendChild(thisProduct.element);
+      
     }
   }
 
